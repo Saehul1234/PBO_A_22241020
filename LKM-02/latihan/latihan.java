f56910f335b18eb03e8557471a543f5e00083151
Saehul1234 <128339916+Saehul1234@users.noreply.github.com>
2023-11-29 09:54:14 +0800

    Add files via upload

---
+++ LKM-02/latihan/latihan.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author HILMI
+ */
+abstract class KarakterKartun {
+    String nama;
+    public abstract void menyanyi();
+    public abstract void ketawa();
+}
+// Interface
+interface Pahlawan {
+    public void terbang(); // interface method (does not have a body)
+    public void berlari(); // interface method (does not have a body)
+}
+interface Penduduk {
+    public void berlari(); // interface method (does not have a body)
+}
+
+
