6218f2e27afc1fc19293addda37b4342ce34f690
Saehul1234 <128339916+Saehul1234@users.noreply.github.com>
2023-12-20 09:36:37 +0800

    Add files via upload

---
+++ seter and geter/src/com/tutorial/Main.java
@@ -0,0 +1,32 @@
+package com.tutorial;
+
+
+class User {
+   private String username;
+   private String password;
+
+   // membuat konstruktor
+   User(String username, String password){
+      this.username = username;
+      this.password = password;
+
+   }
+   // method getter
+   public String getusername(){
+      return this.username;
+   }
+
+   public String getpassword(){
+      return this.password;
+   } 
+}
+public class Main {
+ public static void main(String[] args) {
+    //instansi obkek
+    User person_1 = new User( "Saitama", "123456789");
+
+    // panggil method getter
+    System.out.println(person_1.getusername());
+    System.out.println(person_1.getpassword());
+ }
+}
